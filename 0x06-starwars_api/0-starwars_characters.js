c853c80603d9e13d80fc8b324347975f4cb3b438
agwata9 <agwata9@gmail.com>
2023-06-04 08:08:56 +0000

    correction

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,29 @@
-#!/usr/bin/node
-
 const request = require('request');
-const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
-const movieId = process.argv[2];
-// query API
-request(apiUrl + movieId, (error, response, body) => {
-  if (error) throw error;
-  const characters = JSON.parse(body).characters;
-  showNames(characters);
-});
-// show results on the console
-const showNames = (names, i = 0) => {
-  if (i === names.length) return;
-  request(names[i], (error, response, body) => {
-    if (error) throw error;
-    console.log(JSON.parse(body).name);
-    showNames(names, i + 1);
+
+function printMovieCharacters(movieId) {
+  const url = `https://swapi.dev/api/films/${movieId}/`;
+
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const filmData = JSON.parse(body);
+      const characters = filmData.characters;
+
+      characters.forEach((characterUrl) => {
+        request(characterUrl, (error, response, body) => {
+          if (!error && response.statusCode === 200) {
+            const characterData = JSON.parse(body);
+            console.log(characterData.name);
+          } else {
+            console.log(`Failed to fetch character data for ${characterUrl}`);
+          }
+        });
+      });
+    } else {
+      console.log(`Failed to fetch movie data for movie ID ${movieId}`);
+    }
   });
-};
+}
+
+const movieId = process.argv[2];
+printMovieCharacters(movieId);
+
